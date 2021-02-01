4200911a82b8ada8151d49dffcb3be4ba39a9d68
Hazael Jiménez <viajero25057@innovaccion.mx>
2021-01-31 22:25:50 -0600

    add: Routing part 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  Outlet,
+  useNavigate,
+  useParams
+} from 'react-router-dom';
+
+
+let NotImplemented = () => {
+  return (
+    <>
+      <Link to="/videos">Ir a videos</Link>
+      <h1>Esta página aún no está lista</h1>
+    </>
+  )
+}
+
+let Error404 = () => {
+  return (
+    <>
+      <Link to="/">Reresar al inicio</Link>
+      <h1>Esta página no existe - 404</h1>
+    </>
+  )
+}
+
+
+
+const UsuariosOutlet = () => {
+  let navigate = useNavigate();
+  let redirect = () =>{
+    navigate('/')
+  }
+
+  return (
+    <>
+      <button onClick={redirect}>Ir al home</button>
+      <Outlet />
+    </>
+  )
+}
+
+let VideoShow = () => {
+  let { id } = useParams()
+  return(
+    <p>{id}</p>
+  )
+}
 
 function App() {
+  const isAuth = false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NotImplemented />} />
+        
+        <Route path="/usuarios" element={isAuth ? <Navigate to="/" /> : <UsuariosOutlet />}>
+          <Route path="registro" element={<NotImplemented />} />
+          <Route path="login" element={<NotImplemented />} />  
+          <Route path=":id" element={<NotImplemented />} />
+          <Route path=":id/videos" element={<NotImplemented />} />
+        </Route>
+        
+        <Route path="/videos">
+          <Route path="/" element={<NotImplemented />} />
+          <Route path="nuevo" element={<NotImplemented />} />
+          <Route path=":id" element={<VideoShow />} />
+        </Route>
+
+        <Route path="*" element={<Error404 />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
